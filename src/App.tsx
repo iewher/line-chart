20d4b6599fbd8cc4aced4428de550319deb7db3e
Georgy <goshagang@mail.ru>
2023-04-15 12:33:21 +0500

    create line chart

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import './styles.css'
+
+import { Button } from './Button'
+import { ResponsiveLine, ResponsiveLineCanvas } from '@nivo/line'
+import { inc } from './utils'
+import { generateDrinkStats } from '@nivo/generators'
+import { useState } from 'react'
+
+type Flavor = 'svg' | 'canvas'
+
+type ChartProps = {
+    flavor: Flavor
+    iteration: number
+}
+
+console.clear()
+
+const props = {
+    enableSlices: 'x',
+    margin: { top: 20, right: 20, bottom: 60, left: 80 },
+} as const
+
+function Chart({ flavor }: ChartProps) {
+    const data = generateDrinkStats(9)
+
+    if (flavor === 'canvas') {
+        return <ResponsiveLineCanvas data={data} {...props} />
+    }
+
+    return <ResponsiveLine data={data} {...props} />
+}
+
+export default function App() {
+    const [flavor, setFlavor] = useState<Flavor>('svg')
+    const [iteration, setIteration] = useState(inc)
+
+    return (
+        <div className='App'>
+            <h1>Nivo Line Template</h1>
+            <h2>Fork this template</h2>
+            <Button onClick={() => setIteration(inc)}>Generate Data</Button>
+            <Button
+                onClick={() =>
+                    setFlavor((value) => (value !== 'canvas' ? 'canvas' : 'svg'))
+                }
+                >
+                    Use {flavor === 'svg' ? 'Canvas' : 'SVG'}
+            </Button>
+            <div className='Chart'>
+                <Chart {...{ flavor, iteration }} />
+            </div>
+        </div>
+    )
+}
