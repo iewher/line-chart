dc52acab699dcffad91fbf58a4e7f2e99abfaf13
Georgy <goshagang@mail.ru>
2023-04-17 02:24:46 +0500

    comments added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,14 @@ type HeaderProps = {
   onButtonClick: () => void;
 };
 
+/*
+Через пропсы передаю метод onButtonClick, чтобы при нажатии на кнопку, менялось значение для обьекта с данными
+*/
+
 export default function Header({ onButtonClick }: HeaderProps) {
   return (
     <div className='header'>
+      <h2>Период:</h2>
       <Button onClick={onButtonClick}>Построить график</Button>
     </div>
   );
