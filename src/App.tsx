50789a84d6eacd723c5c3261fa795ec7bae6556e
Georgy <goshagang@mail.ru>
2023-04-16 18:43:09 +0500

    The system for obtaining data for the chart has been redesigned

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import './style/styles.css'
 
 import Header from './components/Header'
-import { Button } from './components/Button'
-import { ResponsiveLine, ResponsiveLineCanvas } from '@nivo/line'
+import { ResponsiveLine } from '@nivo/line'
 import { inc } from './components/utils'
 import { generateDrinkStats } from '@nivo/generators'
 import { useState } from 'react'
@@ -22,7 +21,34 @@ const props = {
 } as const
 
 function Chart({ flavor }: ChartProps) {
-    const data = generateDrinkStats(24)
+    const data = [
+        {
+    /*
+    Создаю цикл, который генерирует время по X
+    Этот цикл создает массив с одним обьектом, содержащим массив данных для графика
+    Внутри этого массива мы передаем значение 24, что означает, что он создаст 24 обьекта, и каждый из них со свойствами x, y
+    Изначально я написал, так:
+    { x: '00:00', y: Math.round(Math.random() * 100)},
+    { x: '01:00', y: Math.round(Math.random() * 100)},
+    { x: '02:00', y: Math.round(Math.random() * 100)},
+    ...
+    { x: '23:00', y: Math.round(Math.random() * 100)},
+    */
+          id: 'Скорость',
+          data: Array.from({ length: 24 }, (_, i) => ({
+            x: `${i.toString().padStart(2, '0')}:00`,
+            y: Math.round(Math.random() * 100)
+          }))
+        }
+      ];
+
+    /*
+    Проверяю поступившие данные
+    Кидаю бряку в консоли на return <ResponsiveLine data={data} {...props} />
+    Выдает все правильно
+    */
+
+    console.log(data)
 
     return <ResponsiveLine data={data} {...props} />
 }
