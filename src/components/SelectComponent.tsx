548fecbff72083257570b16ebfc918c312668c9d
Georgy <goshagang@mail.ru>
2023-04-19 03:55:31 +0500

    added comments

--- src/components/SelectComponent.tsx
+++ src/components/SelectComponent.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import Select from 'react-select'
 
+/*
+Указываем типы для OptionType
+*/
+
 type OptionType = {
     value: string
     label: string
 };
+
+/*
+Указываем типы для SelectProps
+*/
 
 type SelectProps = {
     options: OptionType[];
     selectedOption: OptionType | null;
     handleChange: (selectedOption: OptionType | null) => void;
 };
+
+/*
+Создаем реакт компонент SelectComponent, который создает дропдаун список для направлений,
+далее вызываем его в хедере
+*/
 
 const SelectComponent: React.FC<SelectProps> = ({
     options,
