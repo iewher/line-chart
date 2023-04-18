1b2de9f08e0cedd1ba6e0b40c3020758da6041af
Georgy <goshagang@mail.ru>
2023-04-18 23:52:20 +0500

    directions are now called here

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from './Button';
+import SelectComponent from './SelectComponent'
 
 type HeaderProps = {
   onButtonClick: () => void;
@@ -29,25 +30,45 @@ const timeOptions = Array.from({ length: 24 }, (_, i) => ({
 export default function Header({ onButtonClick }: HeaderProps) {
   const [startInterval, setStartInterval] = useState('');
   const [endInterval, setEndInterval] = useState('');
+  const [selectedOption, setSelectedOption] = useState<{ value: string, label: string } | null>(null)
+
+  const handleChange = (options: any) => {
+      setSelectedOption(options)
+  }
+
+  const options = [
+      {value: '1', label: 'Направление 1'},
+      {value: '2', label: 'Направление 2'},
+      {value: '3', label: 'Направление 3'},
+      {value: '4', label: 'Направление 4'},
+      {value: '5', label: 'Направление 5'}
+  ]
 
   return (
     <div className='header'>
       <h2>Период:</h2>
       <h2 className='from'>с:</h2>
-        <Select 
-          value={startInterval}
-          onChange={(e) => setStartInterval(e.target.value)}
-          options={timeOptions}
+      <Select 
+        value={startInterval}
+        onChange={(e) => setStartInterval(e.target.value)}
+        options={timeOptions}
         />
       <h2 className='to'>до:</h2>
-        <Select 
-          value={endInterval}
-          onChange={(e) => setEndInterval(e.target.value)}
-          options={timeOptions}
+      <Select 
+        value={endInterval}
+        onChange={(e) => setEndInterval(e.target.value)}
+        options={timeOptions}
         />
       <h2 className='interval'>Интервал:</h2>
+      <div className='Directions'>
+        <h2>Направления:</h2>
+          <SelectComponent 
+          options={options}
+          selectedOption={selectedOption}
+          handleChange={handleChange}
+          />
+      </div>
         <Button onClick={onButtonClick}>Построить график</Button>
-      
     </div>
   );
 }
